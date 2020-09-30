72ea290aa365cce07675f3b1292f2012bc5451c3
mikebernal <mikebernal.me@gmail.com>
2020-09-30 13:17:50 +0800

    cognito pool added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit, OnDestroy } from '@angular/core';
+import { onAuthUIStateChange, CognitoUserInterface, AuthState } from '@aws-amplify/ui-components';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sports-management-ui';
+
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'amplify-angular-auth';
+  user: CognitoUserInterface | undefined;
+  authState: AuthState;
+  isAdmin: string;
+
+  constructor(private ref: ChangeDetectorRef) { }
+
+  // Needs to access DOM props only when needed because of authentication rules
+  ngOnInit() {
+
+    onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData as CognitoUserInterface;
+      this.ref.detectChanges();
+
+      if (authState === AuthState.SignedIn) {
+        this.isAdmin = this.user.signInUserSession.accessToken.payload['cognito:groups'].find(group => group === 'Admin');
+        console.log('user successfully signed in!');
+        console.log('user data: ', authData);
+        console.log('authState: ', authState);
+        console.log('Has Admin rights: ', this.isAdmin);
+      }
+      if (!authData) {
+        console.log('user is not signed in...');
+      }
+    });
+
+  }
+
+  ngOnDestroy() {
+    return onAuthUIStateChange;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+import Amplify from 'aws-amplify';
+import awsconfig from '../aws-exports';
+
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
-import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+Amplify.configure(awsconfig);
 
 @NgModule({
   declarations: [
     AppComponent,
-      GameComponent,
-      LoginComponent
+    GameComponent,
+    PageNotFoundComponent
    ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    AmplifyUIAngularModule,
+    RouterModule.forRoot([
+      { path: '**', component: PageNotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
